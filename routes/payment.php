f6ab93cb83107346f4fb52b8d8924d0be6282869
Bùi Thế Dũng <dungbt@cppsw.com>
2023-02-03 11:16:37 +0700

    first commit

---
+++ routes/payment.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PaymentController;
+
+
+
+Route::group(['prefix' => 'payment', 'as'=>'payment.'], function($route){
+    $route->any('/vnpay', [ PaymentController::class, 'create'])->name('create');
+    $route->any('/return-back',[PaymentController::class, 'return'])->name('return');
+
+});
