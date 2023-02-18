cd73d287d6e3d160d443b6a2feb6a1e48c376d33
Bùi Thế Dũng <dungbt@cppsw.com>
2023-02-18 16:21:53 +0700

    add check time out in previous page

--- resources/views/checkout/waitingPayment.blade.php
+++ resources/views/checkout/waitingPayment.blade.php
@@ -8,27 +8,39 @@
         <div class="payments row justify-content-center" style="width:100%;">
             <div class="payment-text-sec d-block justify-content-center" style="padding: 0px 50px;">
                 <div class="payment-info">
-                    <p class="payment-info-text">Chuyển khoản đến {{PAYMENT_METHOD_NAME[$paymentMethod->payment_id]}}</p>
+                    <p class="payment-info-text">Chuyển khoản đến {{ PAYMENT_METHOD_NAME[$paymentMethod->payment_id] }}</p>
                 </div>
 
                 <div class="payment-info">
                     <p class="payment-info-title">Số tài khoản:</p>
-                    <span class="d-flex justify-content-start"><p class="payment-info-text" id="bankAccountNumber">{{$paymentMethod->bank_account_number}}</p>&emsp;<i class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountNumber')"></i></span>
+                    <span class="d-flex justify-content-start">
+                        <p class="payment-info-text" id="bankAccountNumber">{{ $paymentMethod->bank_account_number }}</p>
+                        &emsp;<i class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountNumber')"></i>
+                    </span>
                 </div>
 
                 <div class="payment-info">
                     <p class="payment-info-title">Người thụ hưởng:</p>
-                    <span class="d-flex justify-content-start"><p class="payment-info-text" id="bankAccountName">{{$paymentMethod->name_receiver}}</p>&emsp;<i class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountName')"></i></span>
+                    <span class="d-flex justify-content-start">
+                        <p class="payment-info-text" id="bankAccountName">{{ $paymentMethod->name_receiver }}</p>&emsp;<i
+                            class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountName')"></i>
+                    </span>
                 </div>
 
                 <div class="payment-info">
                     <p class="payment-info-title">Nội dung:</p>
-                    <span class="d-flex justify-content-start"><p class="payment-info-text" id="bankAccountContent">{{$checkoutCode}}</p>&emsp;<i class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountContent')"></i></span>
+                    <span class="d-flex justify-content-start">
+                        <p class="payment-info-text" id="bankAccountContent">{{ $checkoutCode }}</p>&emsp;<i
+                            class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('bankAccountContent')"></i>
+                    </span>
                 </div>
 
                 <div class="payment-info">
                     <p class="payment-info-title">Số tiền cần thanh toán:</p>
-                    <span class="d-flex justify-content-start"><p class="payment-info-text" id="paymentMoney">{{$paymentMoney}}</p>VND&emsp;<i class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('paymentMoney')"></i></span>
+                    <span class="d-flex justify-content-start">
+                        <p class="payment-info-text" id="paymentMoney">{{ $paymentMoney }}</p>VND&emsp;<i
+                            class="fa-solid fa-copy fa-xl copy-icon" onclick="copyText('paymentMoney')"></i>
+                    </span>
                 </div>
 
                 <div class="payment-info">
@@ -41,13 +53,16 @@
                 </div>
 
                 <div class="payment-info">
-                    <span class="d-flex justify-content-start"><div class="four-dots" style="height:30px; width: 30px; margin:5px 0px;"></div><span>Đang đợi thanh toán</span></span>
+                    <span class="d-flex justify-content-start">
+                        <div class="four-dots" style="height:30px; width: 30px; margin:5px 0px;"></div><span>Đang đợi thanh
+                            toán</span>
+                    </span>
                 </div>
 
             </div>
 
             <div class="payment-qr-sec d-flex justify-content-center" style="padding: 0px 50px;">
-                <img src = "{{$paymentMethod->qr_url}}" style="width:100%; height: 300px;">
+                <img src="{{ $paymentMethod->qr_url }}" style="width:100%; height: 300px;">
             </div>
 
 
@@ -55,15 +70,27 @@
 
         </div>
     </div>
-
 @endsection
 
 @section('script')
-    @vite(['resources/sass/payment.scss','resources/js/bootstrap.js'])
+    @vite(['resources/sass/payment.scss', 'resources/js/bootstrap.js'])
 @endsection
 
 @section('script2')
     <script type="text/javascript">
+        window.history.pushState({
+            page: 1
+        }, "", "");
+
+        window.onpopstate = function(event) {
+            if (event) {
+                window.location.href = `{{ route('checkout.checkoutTimeOut', '') }}` + "/" +
+                        {{ $checkoutId }};
+                console.log('back btn clicked');
+                // Code to handle back button or prevent from navigation
+            }
+        }
+
         function copyText(id) {
             var copyText = document.getElementById(id).textContent;
 
@@ -106,7 +133,8 @@
                 /* Stop the countdown when the time reaches 0 */
                 if (time < 0) {
                     clearInterval(countdown);
-                    window.location.href = `{{route('checkout.checkoutTimeOut','')}}` +"/"+{{$checkoutId}};
+                    window.location.href = `{{ route('checkout.checkoutTimeOut', '') }}` + "/" +
+                        {{ $checkoutId }};
                 }
             }, 1000);
 
@@ -114,8 +142,8 @@
         });
 
 
-        const channelName = 'public.payment.' + '{{$checkoutCode}}';
-        const redirectRoute = `{{route('checkout.checkoutComplete','')}}` +"/"+{{$checkoutId}};
+        const channelName = 'public.payment.' + '{{ $checkoutCode }}';
+        const redirectRoute = `{{ route('checkout.checkoutComplete', '') }}` + "/" + {{ $checkoutId }};
         const waitingPayment = true;
     </script>
 @endsection
